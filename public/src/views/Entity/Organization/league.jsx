d9767ea30721723046575ca8748e96fe2d7cd56f
MedRochon <77286197+MedRochon@users.noreply.github.com>
2021-01-29 11:18:51 -0500

    Update public/src/views/Entity/Organization/league.jsx
    Co-authored-by: julienbernat <57548160+julienbernat@users.noreply.github.com>

--- public/src/views/Entity/Organization/league.jsx
+++ public/src/views/Entity/Organization/league.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 
-import { IgContainer, Icon, HeaderHomeOrg } from '../../../components/Custom';
+import IgContainer from '../../../components/Custom/IgContainer';
+import Icon from '../../../components/Custom/Icon';
+import HeaderHomeOrg from '../../../components/Custom/HeaderHomeOrg';
 import { ENTITIES_ROLE_ENUM } from '../../../../common/enums';
 import { formatPageTitle } from '../../../utils/stringFormats';
 import { useTranslation } from 'react-i18next';
