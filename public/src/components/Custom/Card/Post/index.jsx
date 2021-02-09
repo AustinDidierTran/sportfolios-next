090b77494f7ce5a92e3d978b42acdb8e445fce70
Mederic <med.rochon@gmail.com>
2021-02-09 13:26:43 -0500

    Diviser en component et permettre de like

--- public/src/components/Custom/Card/Post/index.jsx
+++ public/src/components/Custom/Card/Post/index.jsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 
 import Card from '@material-ui/core/Card';
 import styles from './Post.module.css';
+import PostComment from '../../Posts/comment';
 
 export default function Post(props) {
   const { postInfo } = props;
-  return <Card className={styles.card}>{postInfo.content}</Card>;
+  return (
+    <Card className={styles.card}>
+      {postInfo.content}
+      <PostComment isLiked={postInfo.liked} postId={postInfo.id} />
+    </Card>
+  );
 }
