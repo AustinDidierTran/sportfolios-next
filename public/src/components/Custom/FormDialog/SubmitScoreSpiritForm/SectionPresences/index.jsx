d9e45cfde59f8efd1c98b9dd72e7df1633c71670
MedRochon <77286197+MedRochon@users.noreply.github.com>
2021-02-25 10:55:53 -0500

    Revert "Wcs 848/submit score detailed (#87)"
    This reverts commit c96305775c5bf02eac5f23b842396c00db352a85.

--- public/src/components/Custom/FormDialog/SubmitScoreSpiritForm/SectionPresences/index.jsx
+++ public/src/components/Custom/FormDialog/SubmitScoreSpiritForm/SectionPresences/index.jsx
@@ -8,11 +8,9 @@ import { ACTION_ENUM, Store } from '../../../../../Store';
 
 import styles from '../SubmitScoreSpiritForm.module.css';
 import { formatRoute } from '../../../../../../common/utils/stringFormat';
-
 import Button from '../../../Button';
 import IconButton from '../../../IconButton';
 import MultiSelect from '../../../MultiSelect';
-
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 
