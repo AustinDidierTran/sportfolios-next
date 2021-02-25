437daf9a64e3d57235e9773b6ab3e3bf9c71e229
MedRochon <77286197+MedRochon@users.noreply.github.com>
2021-02-25 11:05:47 -0500

    Revert "Wcs 848/submit score detailed (#87)" (#92)
    This reverts commit c96305775c5bf02eac5f23b842396c00db352a85.

--- public/src/tabs/Schedule/index.jsx
+++ public/src/tabs/Schedule/index.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import AllGames from './AllGames';
-import GameDetailed from './GameDetailed';
-import { useRouter } from 'next/router';
 
 export default function ScheduleTab() {
-  const router = useRouter();
-  const { gameId } = router.query;
-
-  if (gameId) {
-    return <GameDetailed gameId={gameId} />;
-  }
   return <AllGames />;
 }
