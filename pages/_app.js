654e2a96215d18ccff0c45941abee985699898da
julienbernat <57548160+julienbernat@users.noreply.github.com>
2021-03-08 16:01:49 -0500

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -69,7 +69,6 @@ function MyApp({ Component, pageProps }) {
           </Elements>
         </ThemeProvider>
       </I18nextProvider>
-      <script type="module" src="https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate"></script>
     </StoreProvider>
   );
 }
