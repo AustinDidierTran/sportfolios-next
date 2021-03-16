c2f9fe0a03407fd4418c2d7174f0986f9b7b4154
achille lanctot saumure <achille.lanctots@gmail.com>
2021-03-16 14:58:56 -0400

    fixes pour la vue joueur

--- public/src/tabs/Rankings/Ranking/index.jsx
+++ public/src/tabs/Rankings/Ranking/index.jsx
@@ -8,6 +8,7 @@ export default function Ranking(props) {
 
   const [items, setItems] = useState([]);
 
+  // console.log({rankiiings : ranking, title: title});
   const getItems = () => {
     if (withStats) {
       const items = ranking.map((r, index) => ({
