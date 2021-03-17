f20bdf2ca6d9a91b6b7d241368f32be320ac758a
achille lanctot saumure <achille.lanctots@gmail.com>
2021-03-17 10:34:13 -0400

    little fixes

--- public/src/tabs/Rankings/Ranking/index.jsx
+++ public/src/tabs/Rankings/Ranking/index.jsx
@@ -8,7 +8,6 @@ export default function Ranking(props) {
 
   const [items, setItems] = useState([]);
 
-  // console.log({rankiiings : ranking, title: title});
   const getItems = () => {
     if (withStats) {
       const items = ranking.map((r, index) => ({
