3194cbb5f0ee4ff6c8ed83a046ce0735044b8a59
achille lanctot saumure <achille.lanctots@gmail.com>
2021-04-12 15:01:53 -0400

    si les scores des équipes sont tous a 0, elles sont placées selon leur position initial

--- public/src/tabs/EditRankings/FinalRanking/index.jsx
+++ public/src/tabs/EditRankings/FinalRanking/index.jsx
@@ -52,41 +52,6 @@ export default function FinalRanking(props) {
         phaseId: phase.id,
       })
     );
-
-    // let teams = [];
-    // let position = 1;
-    // games.forEach((g) => {
-    //   if (!teams.filter((team) => team.id === g.teams[0].team_id).length) {
-    //     teams.push({
-    //       name: g.teams[0].name,
-    //       roster_id: g.teams[0].roster_id,
-    //       id: g.teams[0].team_id,
-    //       position,
-    //     });
-    //     position = position + 1;
-    //   }
-    //   if (!teams.filter((team) => team.id === g.teams[1].team_id).length) {
-    //     teams.push({
-    //       name: g.teams[1].name,
-    //       roster_id: g.teams[1].roster_id,
-    //       id: g.teams[1].team_id,
-    //       position,
-    //     });
-    //     position = position + 1;
-    //   }
-    // });
-    // games.map((game) => {
-    //   const res1 = teams.find((t) => game.teams[0].team_id === t.id);
-    //   game.teams[0].position = res1.position;
-
-    //   const res2 = teams.find((t) => game.teams[1].team_id === t.id);
-    //   game.teams[1].position = res2.position;
-    // });
-    // allTeams.forEach((t) => {
-    //   if (!teams.map((t) => t.id).includes(t.teamId)) {
-    //     teams.push({ name: t.name, position: t.initial_position, roster_id: t.roster_id, id: t.teamId });
-    //   }
-    // });
     const teams = allTeams.map((team) => {
       let positionName = `${team.origin_position}. ${team.phaseName}`;
       if (team.origin_phase === prerankPhaseId) {
--- public/src/tabs/Rankings/PhaseRanking/index.jsx
+++ public/src/tabs/Rankings/PhaseRanking/index.jsx
@@ -14,6 +14,7 @@ export default function PhaseRankings(props) {
   const { t } = useTranslation();
 
   const [phases, setPhases] = useState([]);
+  const [isTeamsScoreEqual, setIsTeamScoreEqual] = useState(false);
 
   const getPhases = async () => {
     const { data: phases } = await api(
@@ -49,13 +50,6 @@ export default function PhaseRankings(props) {
           });
           return { ranking, title: phase.name, subtitle: t('phase_not_started'), status: phase.status };
         }
-
-        //   phase.ranking
-        //     .map((r) => ({ ...r, name: `${r.initial_position}. ${phase.name}` }))
-        //     .sort((a, b) => a.initial_position - b.initial_position);
-        //   return { ranking, title: phase.name, subtitle: t('phase_not_started'), status: phase.status };
-        // }
-
         const {
           data: { games, teams: allTeams },
         } = await api(
@@ -64,40 +58,6 @@ export default function PhaseRankings(props) {
             phaseId: phase.id,
           })
         );
-        // let teams = [];
-        // let position = 1;
-        // games.forEach((g) => {
-        //   if (!teams.filter((team) => team.id === g.teams[0].team_id).length) {
-        //     teams.push({
-        //       name: g.teams[0].name,
-        //       roster_id: g.teams[0].roster_id,
-        //       id: g.teams[0].team_id,
-        //       position,
-        //     });
-        //     position = position + 1;
-        //   }
-        //   if (!teams.filter((team) => team.id === g.teams[1].team_id).length) {
-        //     teams.push({
-        //       name: g.teams[1].name,
-        //       roster_id: g.teams[1].roster_id,
-        //       id: g.teams[1].team_id,
-        //       position,
-        //     });
-        //     position = position + 1;
-        //   }
-        // });
-        // games.map((game) => {
-        //   const res1 = teams.find((t) => game.teams[0].team_id === t.id);
-        //   game.teams[0].position = res1.position;
-
-        //   const res2 = teams.find((t) => game.teams[1].team_id === t.id);
-        //   game.teams[1].position = res2.position;
-        // });
-        // allTeams.forEach((t) => {
-        //   if (!teams.map((t) => t.id).includes(t.teamId)) {
-        //     teams.push({ name: t.name, position: t.initial_position, roster_id: t.roster_id, id: t.teamId });
-        //   }
-        // });
         const teams = allTeams.map((team) => {
           let positionName = `${team.origin_position}. ${team.phaseName}`;
           if (team.origin_phase === prerankPhaseId) {
@@ -116,7 +76,12 @@ export default function PhaseRankings(props) {
 
         const rankingStats = ranking.map((r) => {
           const t = teams.find((t) => t.id === r.id);
-          return { ...r, positionName: t.positionName, finalPosition: t.finalPosition };
+          return {
+            ...r,
+            positionName: t.positionName,
+            finalPosition: t.finalPosition,
+            initialPosition: t.initial_position,
+          };
         });
 
         if (phase.status === PHASE_STATUS_ENUM.DONE) {
@@ -127,6 +92,15 @@ export default function PhaseRankings(props) {
             subtitle: t('phase_done'),
             status: phase.status,
           };
+        } else {
+          const playedGames = games.reduce((prev, curr) => {
+            const score1 = curr.teams[0].score;
+            const score2 = curr.teams[1].score;
+            return prev.concat([score1, score2]);
+          }, []);
+          if (playedGames.some((g) => g > 0)) {
+            setIsTeamScoreEqual(true);
+          }
         }
 
         return {
@@ -157,6 +131,7 @@ export default function PhaseRankings(props) {
               ranking={phase.ranking}
               title={phase.title}
               subtitle={phase.subtitle}
+              allTeamsEqual={isTeamsScoreEqual}
               withStats
             ></Ranking>
           ) : (
