5df9d3859e3fe38d221377c924a36f099481462a
Achilles <62257608+Troy-Boy@users.noreply.github.com>
2021-04-15 15:18:47 -0400

    redirect to the page you were when logging in (#236)
    * redirect to the page you were when logging in
    
    * on revient sur le bon tab

--- public/src/views/Header/LoggedOut/index.jsx
+++ public/src/views/Header/LoggedOut/index.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
-import  styles from './LoggedOut.module.css';
+import styles from './LoggedOut.module.css';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { goTo, ROUTES } from '../../../actions/goTo';
 import Button from '../../../components/Custom/Button';
 
-
 export default function LoggedOut(props) {
   const { t } = useTranslation();
-  const {isNotLogin} = props;
+  const router = useRouter();
+
+  const { isNotLogin } = props;
 
   return (
     <div>
       <AppBar position="static" style={{ position: 'fixed', top: 0 }}>
         <Toolbar>
-          <Typography className={styles.title} style={{ fontSize: '1.5rem', color: 'white'}} noWrap>
-            <Link href={ROUTES.home} className={styles.link}>Sportfolios</Link>
+          <Typography className={styles.title} style={{ fontSize: '1.5rem', color: 'white' }} noWrap>
+            <Link href={ROUTES.home} className={styles.link}>
+              Sportfolios
+            </Link>
           </Typography>
-            {isNotLogin && 
-              (
-                <div className={styles.login}>
-                  <Button className={styles.button} disableElevation onClick={() => {
-                    goTo(ROUTES.login);
-                  }}>
-                    {t('connection')}
-                  </Button> 
-                </div>
-              )    
-            }
+          {isNotLogin && (
+            <div className={styles.login}>
+              <Button
+                className={styles.button}
+                disableElevation
+                onClick={() => {
+                  const redirectUrl = router.asPath;
+                  goTo(ROUTES.login, null, { redirectUrl });
+                }}
+              >
+                {t('connection')}
+              </Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
