44a5cdaf485cede2958453a1d3d3f3b825a9fb31
julienbernat <57548160+julienbernat@users.noreply.github.com>
2021-05-03 13:56:32 -0400

    New useWindowSize hooks handles everything

--- public/src/components/Custom/Tab/index.jsx
+++ public/src/components/Custom/Tab/index.jsx
@@ -1,27 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Tab from '@material-ui/core/Tab';
 import CustomIcon from '../Icon';
 import { useWindowSize } from '../../../hooks/window';
 
 export default function CustomTab(props) {
   const { icon, label, onClick } = props;
-  const [displayText, setDisplayText] = useState(false);
   const [width] = useWindowSize();
 
-
-  const handleResize = () => {
-    setDisplayText(Boolean(width > 600));
-  };
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  if (displayText) {
+  if (width > 600) {
     return <Tab icon={<CustomIcon icon={icon} />} label={label} onClick={onClick} />;
   }
 
