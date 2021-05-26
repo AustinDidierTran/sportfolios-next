81f66f5aa63e3e0faff10fd4e2beeb9815821f15
Pierre-Etienne <pethebuffon@gmail.com>
2021-05-26 13:53:58 -0400

    added the sort on table

--- public/src/tabs/Settings/PlayersRegistered/PlayersRow/index.jsx
+++ public/src/tabs/Settings/PlayersRegistered/PlayersRow/index.jsx
@@ -75,7 +75,7 @@ export default function PlayersRow(props) {
           {player.isMember ? (
             <Chip label={t('yes')} color="primary" variant="outlined" />
           ) : (
-            <Chip label={t('no')} color="secondary" variant="outlined" />
+            <Chip label={t('no.no')} color="secondary" variant="outlined" />
           )}
         </StyledTableCell>
         <StyledTableCell align="center" onClick={handleExpand}>
