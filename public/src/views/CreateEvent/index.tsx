4a823c92f08b809856e9a9f526053e7644b16dd3
julienbernat <57548160+julienbernat@users.noreply.github.com>
2021-06-24 11:25:47 -0400

    Fix Build thank you CI

--- public/src/views/CreateEvent/index.tsx
+++ public/src/views/CreateEvent/index.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GLOBAL_ENUM } from '../../../common/enums';
-import { formatPageTitle } from '../../utils/stringFormats';
 import { useTranslation } from 'react-i18next';
-import { Helmet } from 'react-helmet';
 import dynamic from 'next/dynamic';
+import { formatPageTitle } from '../../utils/stringFormats';
 
 const EntityCreate = dynamic(() => import('../../components/Custom/EntityCreate'));
 
 const CreateEvent: React.FunctionComponent = () => {
   const { t } = useTranslation();
+  useEffect(() => {
+    document.title = formatPageTitle(t('create.create_event'));
+  }, []);
 
-  return (
-    <>
-      <Helmet>
-        <meta property="og:title" content={formatPageTitle(t('create.create_event'))} />
-        <meta property="og:description" content={t('create.create_your_event')} />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="fr_CA" />
-        <title>{formatPageTitle(t('create.create_event'))}</title>
-      </Helmet>
-      <EntityCreate type={GLOBAL_ENUM.EVENT} />
-    </>
-  );
+  return <EntityCreate type={GLOBAL_ENUM.EVENT} />;
 };
 export default CreateEvent;
