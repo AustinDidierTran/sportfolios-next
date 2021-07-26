9de64f6e2853b055ec2c977245f1d7e13b60702f
julienbernat <57548160+julienbernat@users.noreply.github.com>
2021-07-26 13:49:36 -0400

    Last change

--- public/src/actions/service/entity/post.tsx
+++ public/src/actions/service/entity/post.tsx
@@ -64,7 +64,7 @@ export function confirmEmail(token: string): Promise<any> {
     body: JSON.stringify({
       token,
     }),
-  }).then((res) => res);
+  });
 }
 
 export function sendRequestToJoinTeam(teamId: string, personId: string): Promise<number> {
