8399cbbcc47c7a253b48ece07e992a746419ab2c
julienbernat <57548160+julienbernat@users.noreply.github.com>
2021-08-05 10:29:08 -0400

    Fix expand phase

--- public/src/tabs/EditRankings/index.tsx
+++ public/src/tabs/EditRankings/index.tsx
@@ -48,7 +48,7 @@ const EditRankings: React.FunctionComponent = () => {
 
   const [phases, setPhases] = useState<Phase[]>([]);
   const [preranking, setPreranking] = useState<Preranking[]>();
-  const [expandedPhases, setExpandedPhases] = useState<Phase[]>([]);
+  const [expandedPhases, setExpandedPhases] = useState<string[]>([]);
 
   const [phaseToEnd, setPhaseToEnd] = useState<Phase>();
   const [phaseToDelete, setPhaseToDelete] = useState<Phase>();
@@ -363,11 +363,11 @@ const EditRankings: React.FunctionComponent = () => {
   };
 
   const onShrink = (phaseId: string): void => {
-    setExpandedPhases((e) => e.filter((p) => p.phaseId !== phaseId));
+    setExpandedPhases((e) => e.filter((p) => p !== phaseId));
   };
 
   const onExpand = (phaseId: string): void => {
-    setExpandedPhases((e) => e.filter((p) => p.phaseId == phaseId));
+    setExpandedPhases([...expandedPhases, phaseId]);
   };
 
   return (
