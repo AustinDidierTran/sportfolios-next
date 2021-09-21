5003679470bf971ed959f1091ef2a6dbbe8f718a
berg0401 <Gabriel.Bertrand@USherbrooke.ca>
2021-09-21 15:44:23 -0400

    A un bog près

--- public/src/views/AdminPanel/AdminEntitiesView.tsx
+++ public/src/views/AdminPanel/AdminEntitiesView.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 
 import { Teams } from './AdminEntities/teams';
 import { Events } from './AdminEntities/events';
+import { People } from './AdminEntities/people';
+import { Organizations } from './AdminEntities/organizations';
+import Organization from '../Entity/Organization';
 
 const AdminEntitiesView: React.FunctionComponent = () => {
   return (
     <React.Fragment>
       <Teams />
       <Events />
+      <People />
+      <Organizations />
     </React.Fragment>
   );
 };
