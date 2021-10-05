2bb36506d6960fd2b04781ad00bb05be9fc8ee75
ClaudeGP <31514003+ClaudeGP@users.noreply.github.com>
2021-10-05 13:11:05 -0400

    modfication of metadata

--- pages/page/validationAccount/[email].tsx
+++ pages/page/validationAccount/[email].tsx
@@ -22,8 +22,8 @@ const ConfirmationEmailSentRoute: React.FunctionComponent = () => {
   return (
     <>
       <Head>
-        <meta property="og:title" content={t('metadata.confirmationEmailSent.title')} />
-        <meta property="og:description" content={t('metadata.confirmationEmailSent.description')} />
+        <meta property="og:title" content={t('metadata.validationAccount.title')} />
+        <meta property="og:description" content={t('metadata.validationAccount.description')} />
         <meta property="og:image" content={IMAGE_ENUM.SPORTFOLIOS_BANNER} />
       </Head>
       {email ? <ValidationAccount email={email} /> : <LoadingSpinner />}
