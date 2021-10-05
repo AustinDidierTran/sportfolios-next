adbfc929b5faca96147077615781cf1f84de2d76
ClaudeGP <31514003+ClaudeGP@users.noreply.github.com>
2021-10-05 11:41:08 -0400

    add validation code and traduction

--- public/src/i18n/fr/index.js
+++ public/src/i18n/fr/index.js
@@ -602,6 +602,13 @@ export default {
       choose: 'Choisir',
       choose_later_date: 'Veuillez choisir une date ultérieure',
     },
+    code_validation: {
+      code_message: 'Validation du compte de l\'adresse: ',
+      code_length: 'La taille doit être de 6 caractères',
+      only_number: 'Seuls des chiffres sont acceptés',
+      invalide_code: 'Le code n\'est pas valide',
+      value_is_required: 'Une valeur est requise',
+    },
     create: {
       create_a_game: 'Créer une partie',
       create_a_phase: 'Créer une phase',
