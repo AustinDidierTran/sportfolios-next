628294cc9af0e729ba44dcadf3b3fee8c1c86240
ClaudeGP <31514003+ClaudeGP@users.noreply.github.com>
2021-10-07 16:31:04 -0400

    add amplify to changePassword

--- public/src/views/UserSettings/ChangePassword/index.jsx
+++ public/src/views/UserSettings/ChangePassword/index.jsx
@@ -12,6 +12,9 @@ import api from '../../../actions/api';
 import { goTo, ROUTES } from '../../../actions/goTo';
 import { REQUEST_STATUS_ENUM } from '../../../../common/enums';
 
+import { Auth } from 'aws-amplify';
+import '../../../utils/amplify/amplifyConfig.jsx';
+
 export default function ChangePassword() {
   const {
     state: { authToken },
@@ -53,6 +56,32 @@ export default function ChangePassword() {
     validateOnBlur: false,
     onSubmit: async (values, { resetForm }) => {
       const { oldPassword, newPassword } = values;
+      await Auth.currentAuthenticatedUser()
+        .then((user) => {
+          return Auth.changePassword(user, oldPassword, newPassword);
+        })
+        .then((data) => {
+          console.log(data);
+          if (data === 'SUCCESS') {
+            resetForm();
+            dispatch({
+              type: ACTION_ENUM.SNACK_BAR,
+              message: t('password_changed'),
+              severity: 'success',
+            });
+          }
+        })
+        .catch((err) => {
+          if (err.code === REQUEST_STATUS_ENUM.UNAUTHORIZED) {
+            // Token is expired, redirect
+            goTo(ROUTES.login);
+          } else if (err.code === REQUEST_STATUS_ENUM.FORBIDDEN) {
+            // old password doesn't match
+            formik.setFieldError('oldPassword', t('wrong_password'));
+          }
+        });
+
+      /*
       const res = await api('/api/user/changePassword', {
         method: 'POST',
         body: JSON.stringify({
@@ -61,21 +90,7 @@ export default function ChangePassword() {
           newPassword,
         }),
       });
-
-      if (res.status < 300) {
-        resetForm();
-        dispatch({
-          type: ACTION_ENUM.SNACK_BAR,
-          message: t('password_changed'),
-          severity: 'success',
-        });
-      } else if (res.status === REQUEST_STATUS_ENUM.UNAUTHORIZED) {
-        // Token is expired, redirect
-        goTo(ROUTES.login);
-      } else if (res.status === REQUEST_STATUS_ENUM.FORBIDDEN) {
-        // old password doesn't match
-        formik.setFieldError('oldPassword', t('wrong_password'));
-      }
+      */
     },
   });
 
