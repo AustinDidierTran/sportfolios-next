c2d9aa0ed89053e92cb3d6d8aebc85f596d8cef5
berg0401 <Gabriel.Bertrand@USherbrooke.ca>
2021-10-07 11:23:37 -0400

    Promise=Member

--- public/src/actions/service/organization/index.ts
+++ public/src/actions/service/organization/index.ts
@@ -1,12 +1,10 @@
 import { Organization } from '../../../../../typescript/entity';
 import { formatRoute } from '../../../utils/stringFormats';
 import api from '../../api';
+import { Member } from '../../../../../typescript/types';
 
 const BASE_URL = '/api/organization';
 
-/**
- * [SPO-97] Promise should not return any, it should return an array of members
- */
-export function getMembers(id: string, searchQuery: string): Promise<any> {
+export function getMembers(id: string, searchQuery: string): Promise<Member> {
   return api(formatRoute(`${BASE_URL}/members`, null, { id, searchQuery }), { method: 'GET' }).then((res) => res.data);
 }
