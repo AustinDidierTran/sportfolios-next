1c3855c6a7568f83d31d7ff8e02fe0c910f52223
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-11-18 11:15:16 -0500

    Refactored conf.js (#512)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const conf = require('./conf');
+const { CLIENT_BASE_URL, PORT } = require('./conf');
 
 const httpsOptions = {
   key: fs.readFileSync('./certificates/localhost.key'),
@@ -18,9 +18,9 @@ app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(conf.port || 3000, (err) => {
+  }).listen(PORT || 3000, (err) => {
     if (err) throw err;
     // eslint-disable-next-line no-console
-    console.log(`> Ready on ${conf.CLIENT_BASE_URL}`);
+    console.log(`> Ready on ${CLIENT_BASE_URL}`);
   });
 });
