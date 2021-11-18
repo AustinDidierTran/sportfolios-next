00dc530c3314e3991fbb6aa77f2d9bba74a4c496
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-11-18 11:20:17 -0500

    Revert "Refactored conf.js (#512)"
    This reverts commit ddbf06142855b73e819d3ccbbaa0ffe529c8725f.

--- conf-template.js
+++ conf-template.js
@@ -1,14 +1,16 @@
-export const ENVIRONMENT = 'DEV';
-export const API_BASE_URL = 'http://localhost:1337';
-export const CLIENT_BASE_URL = 'https://localhost:3000';
-export const ANDROID_APP_URL = 'app.sportfolios.twa';
-export const TOKEN_SECRET = '942ad648-fd13-4386-a8ea-a0d4e75324ef';
-export const STRIPE_PUBLIC_KEY = 'pk_test_qd1ulz9FxZA3aj2OcBfjqqro00G1K5WrTb';
-export const GOOGLE_ANALYTICS_TRACKING_ID = 'UA-178163285-2';
-export const GOOGLE_PLACES_API_KEY = 'AIzaSyCQaa2Wue1_hO6LkozWukxUFhrh2nwwZ7U';
-export const FACEBOOK_PAGE_ID = '104239018247436';
-export const PORT = 3000;
-export const USER_POOL_ID = 'us-east-2_YFfg7m94Z';
-export const CLIENT_ID = '711jelco0umon99s8gg60qgn09';
-export const IDENTITY_POOL_ID = 'us-east-2:96bdf3db-5e58-432e-9a9f-49f080fc8f03';
-export const REGION = 'us-east-2';
+module.exports = {
+  // OR http://localhost:1337 if you want to point to your local server
+  API_BASE_URL: 'http://localhost:1337',
+  //API_BASE_URL: 'https://private-4f97fa-sportfolios.apiary-mock.com',
+  CLIENT_BASE_URL: 'https://localhost:3000',
+  TOKEN_SECRET: '942ad648-fd13-4386-a8ea-a0d4e75324ef',
+  STRIPE: {
+    publicKey: 'pk_test_qd1ulz9FxZA3aj2OcBfjqqro00G1K5WrTb',
+  },
+  GOOGLE_ANALYTICS_TRACKING_ID: 'UA-178163285-1',
+  GOOGLE_PLACES_API_KEY: 'AIzaSyCQaa2Wue1_hO6LkozWukxUFhrh2nwwZ7U',
+  USER_POOL_ID: 'us-east-2_YFfg7m94Z',
+  CLIENT_ID: '711jelco0umon99s8gg60qgn09',
+  IDENTITY_POOL_ID: 'us-east-2:96bdf3db-5e58-432e-9a9f-49f080fc8f03',
+  REGION: 'us-east-2',
+};
--- feature-flags.js
+++ feature-flags.js
@@ -1,3 +1,1 @@
-import { ENVIRONMENT } from './conf';
-
-export const FEATURE_MESSAGES_ENABLED = ENVIRONMENT === 'DEV' || false;
+export const FEATURE_MESSAGES_ENABLED = true;
