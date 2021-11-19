466bbd4eb162e5fb32d0fb0912f82719846ff9b2
ClaudeGP <31514003+ClaudeGP@users.noreply.github.com>
2021-11-19 10:13:02 -0500

    ajout du dune config

--- feature-flags.js
+++ feature-flags.js
@@ -1,2 +1,5 @@
-export const FEATURE_MESSAGES_ENABLED = false;
-export const FEATURE_GOOGLE_LOGIN = false;
+import conf from "./conf";
+const status = conf.ENVIRONMENT === "dev" ? true : false;
+
+export const FEATURE_MESSAGES_ENABLED = status;
+export const FEATURE_GOOGLE_LOGIN = status;
