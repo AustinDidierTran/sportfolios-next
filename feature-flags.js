a9b8915b52ec3f43a4d6c1e83892cba503502ec8
Austin-Didier Tran <tran.austin.didier@gmail.com>
2021-12-14 11:03:31 -0500

    Update google feature flag

--- feature-flags.js
+++ feature-flags.js
@@ -3,4 +3,4 @@ const ENV_ENUM = { PROD: 1, STAGING: 2, DEV: 3 };
 const env = ENV_ENUM[conf.ENVIRONMENT] || 1;
 
 export const FEATURE_MESSAGES_ENABLED = env >= ENV_ENUM.STAGING;
-export const FEATURE_GOOGLE_LOGIN = env >= ENV_ENUM.DEV;
+export const FEATURE_GOOGLE_LOGIN = env >= ENV_ENUM.STAGING;
